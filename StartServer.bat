b2bd799684f3389c6e13984cd523a38e4538e086
Sean Fung <fengxiang220@gmail.com>
2014-07-13 00:41:46 +0800

    Finish

--- StartServer.bat
+++ StartServer.bat
@@ -3,10 +3,9 @@
 ::需要Java Runtime Environment（JRE）
 ::Author:凤翔（fengxiang220@gmail.com）
 ::Date:2014/1/6
-::Usage:java Server [<port>]  (默认端口号为8888)
+::Usage:java {package name}.Server [<port>]  (默认端口号为8888)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-cd ./com/find1x/jnetcatch/udpattack
-javac Server.java
-cd ../../../../
-java Server
+@cd bin/
+@ECHO UDP服务器已启动
+@java com.find1x.jnetcatch.udpattack.Server
--- StartUDPFloodAttack.bat
+++ StartUDPFloodAttack.bat
@@ -3,11 +3,12 @@
 ::需要Java Runtime Environment（JRE）
 ::Author:凤翔（fengxiang220@gmail.com）
 ::Date:2014/1/6
-::Usage:java UDPFloodAttack <target> <port> <the number of attack threads>
+::Usage:java {package name}.UDPFloodAttack <target> <port> <the number of attack threads>
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-cd ./com/find1x/jnetcatch/udpattack
-javac UDPFloodAttack.java
-cd ../../../../
+@cd ./bin
+@ECHO 开始攻击
+
+::以下为用户修改部分
 ::java UDPFloodAttack ddns.find1x.com 80 3000
-java com.find1x.jnetcatch.udpattack.UDPFloodAttack 192.168.1.4 8888 1
+@java com.find1x.jnetcatch.udpattack.UDPFloodAttack 192.168.1.4 8888 1
